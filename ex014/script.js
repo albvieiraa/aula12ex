da811d1c15854f86e5c6567df4176ba1fc6b35ad
Mary <maryllianbackup@gmail.com>
2023-06-06 12:10:48 -0300

    adicionado a função carregar (horas)

--- ex014/script.js
+++ ex014/script.js
@@ -1,1 +1,21 @@
-alert('Olá')
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 22
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12){
+        // BOM DIA
+        img.src = 'imagens/imagem-manha.png'
+        document.body.style.background = '#DF9816'
+    } else if (hora >= 12 && hora <= 18){
+        //BOA TARDE
+        img.src = 'imagens/imagem-tarde.png'
+        document.body.style.background = '#F9A88D'
+    } else{
+        //BOA NOITE
+        img.src = 'imagens/imagem-noite.png'
+        document.body.style.background = '#1C4E59'
+    }
+}
